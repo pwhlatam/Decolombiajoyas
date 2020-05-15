9337b98b2ce7b5b5b43cfd5b9f8b084f0b12009d
David1100 <acevdo22@hotmail.com>
2020-05-15 09:07:45 -0500

    Actualizacion para borrar desde barra de fav

--- componentes/component_ver_carrito.js
+++ componentes/component_ver_carrito.js
@@ -4,15 +4,35 @@ Vue.component('card-ver-carrito', {
 
     template:
 
-    `
+	`
+	<transition name="slide-fade">
 	<li class="media border border-light p-0">
 	  <img v-bind:src='image' width="20%" class="border-right border-light ">
 	  <div class="media-body ml-3 ">
 	    <h5 class="mt-0 mb-1 pt-3">{{refe}}</h5>
 	    <p class="text-black-50"> {{ gramos }}g aprox </p>
 	  </div>
+	  <div class="btn col-lg-3 border-left border-light p-4">
+        <img v-bind:src='imgdelete' width="15px" @click="deletep(0,id)">
+      </div>
 	</li>  
-    `,
+	<transition>
+	`,
+    data () {
+
+        return {
+
+          imgdelete:'img/delete.svg'
+
+        }
+
+	},
+	  
+	methods:{
+		deletep(add,id){
+			store.commit('addcart',{addp:add,idp:id})  
+		}
+	}
 
 
 })
